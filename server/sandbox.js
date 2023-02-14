70f064a7bc2b1fef8bf86284f45dd662e48278c7
Maryam-Dollet <maryam.dollet@gmail.com>
2023-02-15 00:30:10 +0100

    ml;sm

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./eshops/dedicatedbrand');
+const circlesportswear = require('./eshops/circlesportswear');
 
 async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
   try {
@@ -16,6 +17,21 @@ async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
   }
 }
 
+async function sandbox2 (eshop = 'https://shop.circlesportswear.com/collections/collection-femme') {
+  try {
+    console.log(`🕵️‍♀️  parse ${eshop} eshop`);
+
+    const products = await circlesportswear.scrape(eshop);
+
+    console.log(products);
+    console.log('done');
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
 const [,, eshop] = process.argv;
 
-sandbox(eshop);
+sandbox2(eshop);
