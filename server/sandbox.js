6518fd56938bed8056d24c07fd2c57dfb5abd719
Maryam-Dollet <maryam.dollet@gmail.com>
2023-02-15 19:46:12 +0100

    Update sandbox.js

--- server/sandbox.js
+++ server/sandbox.js
@@ -51,8 +51,42 @@ async function sandbox3 (eshop = 'https://www.montlimart.com/99-vetements') {
   }
 }
 
+async function sandboxall (eshop1 = 'https://www.dedicatedbrand.com/en/men/news', eshop2 = 'https://shop.circlesportswear.com/collections/collection-femme', eshop3 = 'https://www.montlimart.com/99-vetements') {
+  try {
+    console.log(`🕵️‍♀️  browsing ${eshop1} eshop`);
+
+    const products = await dedicatedbrand.scrape(eshop1);
+
+    console.log(products);
+    console.log('done');
+
+    console.log(`🕵️‍♀️  parse ${eshop2} eshop`);
+
+    const products2 = await circlesportswear.scrape(eshop2);
+
+    console.log(products2);
+    console.log('done');
+
+    console.log(`🕵️‍♀️  parse ${eshop3} eshop`);
+
+    const products3 = await montlimart.scrape(eshop3);
+
+    console.log(products3);
+    console.log('done');
+    process.exit(0);
+
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
 const [,, eshop] = process.argv;
 
-//sandbox(eshop);
-//sandbox2(eshop);
+
+sandbox(eshop);
+sandbox2(eshop);
 sandbox3(eshop);
+
+
+//sandboxall(eshop1, eshop2, echop3)
