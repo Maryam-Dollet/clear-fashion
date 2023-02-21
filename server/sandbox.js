3a867590bf0c4951995b2445377c05178174a88d
Maryam-Dollet <maryam.dollet@gmail.com>
2023-02-21 13:33:49 +0100

    Update sandbox.js

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-console, no-process-exit */
+//https://www.dedicatedbrand.com/en/men/all-men#page=16
 
-const FileSystem = require("fs");
+const fs = require("fs");
 const dedicatedbrand = require('./eshops/dedicatedbrand');
 const circlesportswear = require('./eshops/circlesportswear');
 const montlimart = require('./eshops/montlimart');
-const { json } = require("express");
+//const { json } = require("express");
 
 function saveJson(obj){
   FileSystem.writeFile('products.json', JSON.stringify(obj), 'utf8', (error) => {
@@ -12,20 +13,44 @@ function saveJson(obj){
   });
 }
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+async function sandbox(eshop = 'https://www.dedicatedbrand.com/en/men/all-men?p=2') {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
 
     const products = await dedicatedbrand.scrape(eshop);
 
-    //console.log(products);
+    console.log(products);
     console.log('done');
+    console.log(products.length)
     return products;
     //process.exit(0);
   } catch (e) {
     console.error(e);
     process.exit(1);
   }
+}
+
+async function dedicatedDEDICATED(){
+  var products = []
+  for (let i = 0; i <= 16; i++) {
+    link = "https://www.dedicatedbrand.com/en/men/all-men?p="+i
+    /*
+    console.log(`🕵️‍♀️  browsing ${link} eshop`);
+    var partprod = dedicatedbrand.scrape(link)*/
+    var partprod = sandbox(link)
+    var slices = await partprod
+    products = products.concat(slices)
+  }
+  for (let i = 0; i <= 16; i++){
+    link = "https://www.dedicatedbrand.com/en/women/all-women?p="+i
+    var partprod = sandbox(link)
+    var slices = await partprod
+    products = products.concat(slices)
+  }
+  
+  console.log(products)
+  console.log(products.length)
+  return products
 }
 
 async function sandbox2 (eshop = 'https://shop.circlesportswear.com/collections/collection-femme') {
@@ -53,27 +78,34 @@ async function sandbox3 (eshop = 'https://www.montlimart.com/99-vetements') {
     console.log(products);
     console.log('done');
     //process.exit(0);
-    //return products;
+    return products;
   } catch (e) {
     console.error(e);
     process.exit(1);
   }
 }
 
-async function sandboxall (eshop1 = 'https://www.dedicatedbrand.com/en/men/news', eshop2 = 'https://shop.circlesportswear.com/collections/collection-femme', eshop3 = 'https://www.montlimart.com/99-vetements') {
+async function sandboxall (eshop1 = 'https://www.dedicatedbrand.com', eshop2 = 'https://shop.circlesportswear.com', eshop3 = 'https://www.montlimart.com/99-vetements') {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop1} eshop`);
 
-    const products = await dedicatedbrand.scrape(eshop1);
+    const products = await dedicatedDEDICATED();
 
     console.log(products);
     console.log('done');
 
-    console.log(`🕵️‍♀️  parse ${eshop2} eshop`);
+    console.log(`🕵️‍♀️  parse ${eshop2} eshop femmes`);
 
-    const products2 = await circlesportswear.scrape(eshop2);
+    const products2 = await circlesportswear.scrape('https://shop.circlesportswear.com/collections/collection-femme');
 
     console.log(products2);
+    console.log('done');
+
+    console.log(`🕵️‍♀️  parse ${eshop2} eshop hommes`);
+
+    const products2_2 = await circlesportswear.scrape('https://shop.circlesportswear.com/collections/collection-homme');
+
+    console.log(products2_2);
     console.log('done');
 
     console.log(`🕵️‍♀️  parse ${eshop3} eshop`);
@@ -83,19 +115,23 @@ async function sandboxall (eshop1 = 'https://www.dedicatedbrand.com/en/men/news'
     console.log(products3);
     console.log('done');
 
-    var allprods =  products.concat(products2).concat(products3);
+    var allprods =  products.concat(products2).concat(products2_2).concat(products3);
 
+    console.log(allprods.length)
+    
+    
     var objprods = {}
 
     objprods.products = allprods
 
     //console.log(objprods);
 
-    const jsonContent = JSON.stringify(objprods, null, 2);
+    //const jsonContent = JSON.stringify(objprods, null, 2);
+    const jsonContent = JSON.stringify(allprods, null, 2);
 
     //console.log(jsonContent)
 
-    FileSystem.writeFile("output.json", jsonContent, 'utf8', function (err) {
+    fs.writeFile("output.json", jsonContent, 'utf8', function (err) {
       if (err) {
           console.log("An error occured while writing JSON Object to File.");
           return console.log(err);
@@ -103,8 +139,9 @@ async function sandboxall (eshop1 = 'https://www.dedicatedbrand.com/en/men/news'
    
       console.log("JSON file has been saved.");
     });
+    
 
-    process.exit(0);
+    //process.exit(0);
 
   } catch (e) {
     console.error(e);
@@ -112,16 +149,15 @@ async function sandboxall (eshop1 = 'https://www.dedicatedbrand.com/en/men/news'
   }
 }
 
-//const [,, eshop1, eshop2, eshop3] = process.argv;
-const [,, eshop] = process.argv;
 
-/*
-const prods1 = sandbox(eshop);
-let prods2 = sandbox2(eshop);
-let prods3 = sandbox3(eshop);
 
-console.log(prods1);
-*/
-//sandboxall(eshop1, eshop2, eshop3)
+const [,, eshop1, eshop2, eshop3] = process.argv;
+//const [,, eshop] = process.argv;
 
-sandbox3("https://www.montlimart.com/72-nouveautes");
+//sandbox(eshop)
+
+sandboxall(eshop1, eshop2, eshop3)
+
+//sandbox3("https://www.montlimart.com/72-nouveautes");
+
+//dedicatedDEDICATED()
