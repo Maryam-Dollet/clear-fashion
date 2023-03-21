ea3afa719b206bc23b654e3be56cd6308e138990
Maryam-Dollet <maryam.dollet@gmail.com>
2023-03-21 15:43:30 +0100

    Update web.js

--- client/web/web.js
+++ client/web/web.js
@@ -1,7 +1,9 @@
 // Invoking strict mode https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode#invoking_strict_mode
 'use strict';
 
-
+let currentProducts = [];
+let currentPagination = {};
+let currentBrands = [];
 
 /**
  * Fetch products from api
@@ -10,20 +12,67 @@
  * @return {Object}
  */
 const fetchProducts = async () => {
-    try {
-      const response = await fetch(
-        `https://clear-fashion-63uk4zzaz-maryamdollet-gmailcom.vercel.app/products`
-      );
-      const body = await response.json();
+  try {
+    const response = await fetch(
+     `https://clear-fashion-topaz-seven.vercel.app/products`
+    );
+    const body = await response.json();
   
-      if (body.success !== true) {
-        console.error(body);
-        return {currentProducts, currentPagination};
-      }
-  
-      return body.data;
-    } catch (error) {
-      console.error(error);
+    if (body.success !== true) {
+      console.error(body);
       return {currentProducts, currentPagination};
     }
-  };
+  
+  return body.data;
+    } catch (error) {
+    console.error(error);
+    return {currentProducts, currentPagination};
+  }
+};
+
+const fetchBrands = async () => {
+  try {
+    const response = await fetch(
+      `https://clear-fashion-topaz-seven.vercel.app/brands`
+    );
+    const body = await response.json();
+
+    if (body.success !== true) {
+      console.error(body);
+      return {currentBrands};
+    }
+
+    return body.data.result;
+  } catch (error) {
+    console.error(error);
+    return {currentBrands};
+  }
+};
+
+const renderBrands = brands => {
+  const fragment = document.createDocumentFragment();
+  const div = document.createElement('div');
+  const template = brands
+    .map(product => {
+      return `
+      <div>
+        ${product}
+      </div>
+     `;
+    })
+    .join('');
+
+  div.innerHTML = template;
+  fragment.appendChild(div);
+  sectionProducts.innerHTML = '<h2>Products</h2>';
+  sectionProducts.appendChild(fragment);
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
+  //const products = await fetchProducts();
+  const brands = await fetchBrands();
+
+  
+
+});
+
